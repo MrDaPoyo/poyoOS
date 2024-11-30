bade304b7b0813370d954b608b5d0559a7cbd750
poyo <poyo@poyopc.poyonet>
2024-11-30 15:16:55 +0100

    Works!

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,15 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    loop {
+        const VGA_BUFFER: *mut u8 = 0xb8000 as *mut u8;
+
+        let hello = b"Hello, World!";
+        for (i, &byte) in hello.iter().enumerate() {
+            unsafe {
+                *VGA_BUFFER.offset(i as isize * 2) = byte;
+                *VGA_BUFFER.offset(i as isize * 2 + 1) = 0xb; // Light cyan color
+            }
+        }
+    }
 }
